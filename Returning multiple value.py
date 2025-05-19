09baa79a99f61256bb71568230ed19dfd60fef1a
CAROSINE <moshiur287rahman@gmail.com>
2025-05-20 00:34:01 +0600

    Retun value by function

---
+++ Returning multiple value.py
@@ -0,0 +1,11 @@
+def get_student_info():
+    name = "Prothom AIUB"
+    age = "22"
+    major = "CSE"
+    return name, age, major
+
+student_name, student_age, student_major = get_student_info()
+
+print(f"Name: {student_name}")
+print(f"Age: {student_age}")
+print(f"Major: {student_major}")
