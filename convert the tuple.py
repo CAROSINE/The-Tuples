1d681d4118ea7a75337f3a6379a80550715358cd
CAROSINE <moshiur287rahman@gmail.com>
2025-05-21 09:51:29 +0600

    convert tuples

---
+++ convert the tuple.py
@@ -0,0 +1,8 @@
+person = ("Moshiur", 21, "Problem solver_CP", "Never be demotivated by a well-cooked problem solver")
+person_dict = {
+               "name" : person [0],
+               "age" : person [1],
+               "dream" : person [2],
+               "potential" : person [3]
+              }
+print(person_dict)
