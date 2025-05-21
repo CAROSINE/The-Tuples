6ae4048c5e282a2daad38035a3e42f9f50e8c194
CAROSINE <moshiur287rahman@gmail.com>
2025-05-21 09:34:38 +0600

    Create a dictionary student with keys: name, age, courses

---
+++ key-value pairs.py
@@ -0,0 +1,8 @@
+student= {
+                 "name" : "Moshiur",
+                 "age" : "21",
+                 "courses" : ["Math", "Python"]
+         }
+student["age"]=21
+student["grade"] = "A"
+print(f"{student['name']} studies {student['courses']}")
